41c2be4c423d385094542b9e5c003e9e83a2eebd
louisalemar <louisalemar@gmail.com>
2018-12-07 15:05:47 -0500

    add grid to landing page and style header and navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -8,7 +8,7 @@ class Navbar extends Component{
     return(
 	    <div className="demo-big-content">
 	        <Layout>
-	            <Header title="Louis Alemar" scroll>
+	            <Header className="header-color" title="Louis Alemar" scroll>
 	                <Navigation>
 		                  <Link to="/about">About</Link>
 		                  <Link to="/portfolio">Portfolio</Link>
