67cc103d6c51dc2d4bf7ddc23e445c131038cb6e
louisalemar <louisalemar@gmail.com>
2018-12-12 17:47:36 -0500

    add tabs to portfolio page

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import { Tabs, Tab } from 'react-mdl';
 
 class Portfolio extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { activeTab: 0 }
 	}
+	
 	render(){
 		return(
-			<div>
-				<h1>Portfolio Page</h1>
+			<div className="category-tabs">
+				<Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
+					<Tab>React</Tab>
+					<Tab>Angular</Tab>
+					<Tab>MongoDB</Tab>
+				</Tabs>
 			</div>
 		)
 	}
