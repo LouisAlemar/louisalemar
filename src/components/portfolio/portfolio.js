e9a5dc683119191d2083bcc4b0965707f7d08cbd
louisalemar <louisalemar@gmail.com>
2018-12-11 22:57:33 -0500

    add constructor to portfolio class

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class Portfolio extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { activeTab: 0 }
+	}
 	render(){
 		return(
 			<div>
