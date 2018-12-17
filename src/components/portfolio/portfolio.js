3f7e19f9265a3e356b8c4bfea6d4b33a31dfda4a
louisalemar <louisalemar@gmail.com>
2018-12-16 21:44:46 -0500

    add all portfolio cards

--- src/components/portfolio/portfolio.js
+++ src/components/portfolio/portfolio.js
@@ -10,28 +10,54 @@ class Portfolio extends Component {
 	toggleCategories(){
 		if(this.state.activeTab === 0){
 			return(
-				<Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
-					<CardTitle style={{color: '#000', height: '200px', background: 'url(../images/noddle.png) center / cover'}}>
-						Noddle Companies
-					</CardTitle>
-					<CardText>
-						The Noddle Companies is a full service strategic development partner which can provide their clients with assistance in site selection, acquisition master planning, architect and contractor selections and contract negotiation, zoning, infrastructure installation, tax incentives, construction management and more.
-					</CardText>
-					<CardActions className="CardActions" border>
-						<Button colored><a href="http://www.noddlecompanies.com/" target="_blank">Website</a></Button>
-					</CardActions>
-					<CardMenu style={{color: '#fff'}}>
-						<IconButton name="share" />
-					</CardMenu>
-				</Card>
-			)
-		} else if(this.state.activeTab === 1){
-			return(
-				<div><h1>This is Angular</h1></div>
-			)
-		} else if(this.state.activeTab === 2){
-			return(
-				<div><h1>This is MongoDB</h1></div>
+				<div className="projects-grid">
+					<Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+						<CardTitle style={{color: '#000', height: '200px', background: 'url(../images/noddle.png) center / cover'}}>
+							Noddle Companies
+						</CardTitle>
+						<CardText>
+							The Noddle Companies is a full service strategic development partner which can provide their clients with assistance in site selection, acquisition master planning, architect and contractor selections and contract negotiation, zoning, infrastructure installation, tax incentives, construction management and more.
+						</CardText>
+						<CardActions className="CardActions" border>
+							<Button colored><a href="http://www.noddlecompanies.com/" target="_blank">Website</a></Button>
+						</CardActions>
+						<CardMenu style={{color: '#fff'}}>
+							<IconButton name="share" />
+						</CardMenu>
+					</Card>
+
+					<Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+						<CardTitle style={{color: '#000', height: '200px', background: 'url(../images/nycbuildsbio.png) center / cover'}}>
+							NYC Builds Bio
+						</CardTitle>
+						<CardText className="paddingBottom34">
+							Launched in 2018, NYC Builds Bio+ connects commercial life science opportunities to New York City's real estate development community through events, research, reports and educational programs.
+						</CardText>
+						<CardActions className="CardActions" border>
+							<Button colored><a href="http://www.nycbuildsbio.org/" target="_blank">Website</a></Button>
+						</CardActions>
+						<CardMenu style={{color: '#fff'}}>
+							<IconButton name="share" />
+						</CardMenu>
+					</Card>
+
+					<Card shadow={5} style={{minWidth: '450', margin: 'auto'}}>
+						<CardTitle style={{color: '#000', height: '200px', background: 'url(../images/iannelli.png) center / cover'}}>
+							Iannelli Construction
+						</CardTitle>
+						<CardText className="paddingBottom88">
+							An informational website for a construction company.
+						</CardText>
+						<CardActions className="CardActions" border>
+							<Button colored><a href="http://www.iannelliconstruction.com/" target="_blank">Website</a></Button>
+						</CardActions>
+						<CardMenu style={{color: '#fff'}}>
+							<IconButton name="share" />
+						</CardMenu>
+					</Card>
+
+					
+				</div>
 			)
 		}
 	}
@@ -40,20 +66,16 @@ class Portfolio extends Component {
 		return(
 			<div className="category-tabs">
 				<Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
-					<Tab>React</Tab>
-					<Tab>Angular</Tab>
-					<Tab>MongoDB</Tab>
+					<Tab>All</Tab>
 				</Tabs>
 
-				<section className="projects-grid">
-					<Grid className="projects-grid">
-						<Cell col={12}>
-							<div className="content">
-								{this.toggleCategories()}
-							</div>
-						</Cell>
-					</Grid>
-				</section>
+				<Grid>
+					<Cell col={12}>
+						<div className="content">
+							{this.toggleCategories()}
+						</div>
+					</Cell>
+				</Grid>
 			</div>
 		)
 	}
