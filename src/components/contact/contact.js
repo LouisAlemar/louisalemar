cf63a325e1c55562b2fa0497e64283502fa36e31
louisalemar <louisalemar@gmail.com>
2018-12-18 21:17:30 -0500

    lay out contact page first column

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -11,8 +11,9 @@ class Contact extends Component {
 						<img 
 							src={'../images/profile_pic.jpg'} 
 							alt="Louis Alemar"
+							className="profile_pic"
 						/>
-						<p>
+						<p style={{ width: '75%', margin: 'auto', paddingTop: '1rem' }}>
 							Since I was a child, I have always been a creative person. From graduating as a Music Producer to being a Front-End Engineer, the creative aspect behind both careers is what has driven me to pursue these professions. I first taught myself how to build webpages back in the 6th grade. The idea of being able to type lines of code to create something from nothing was mind-boggling to me.
 						</p>
 					</Cell>
