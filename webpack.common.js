a266c7327f8b2c2eb2f09d2efee05230ef1104c3
louisalemar <louisalemar@gmail.com>
2018-12-26 21:46:29 -0500

    deployment testing

---
+++ webpack.common.js
@@ -0,0 +1,71 @@
+// const path = require("path");
+// const webpack = require("webpack");
+
+// module.exports = {
+//   entry: "./src/index.js",
+//   mode: "development",
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js|jsx)$/,
+//         exclude: /(node_modules|bower_components)/,
+//         loader: "babel-loader",
+//         options: { presets: ["@babel/env"] }
+//       },
+//       {
+//         test: /\.css$/,
+//         use: ["style-loader", "css-loader"]
+//       }
+//     ]
+//   },
+//   resolve: { extensions: ["*", ".js", ".jsx"] },
+//   output: {
+//     path: path.resolve(__dirname, "dist/"),
+//     publicPath: "/dist/",
+//     filename: "bundle.js"
+//   },
+//   devServer: {
+//     contentBase: path.join(__dirname, "public/"),
+//     port: 3000,
+//     publicPath: "http://localhost:3000/dist/",
+//     hotOnly: true,
+//     historyApiFallback: true
+//   },
+//   plugins: [new webpack.HotModuleReplacementPlugin()]
+// };
+
+const path = require('path');
+const webpack = require("webpack");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: {
+    app: './src/index.js'
+  },
+  module: {
+    rules: [
+    {
+      test: /\.(js|jsx)$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: "babel-loader",
+      options: { presets: ["@babel/env"] }
+    },
+    {
+      test: /\.css$/,
+      use: ["style-loader", "css-loader"]
+    }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({
+      title: 'Production'
+    })
+  ],
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  }
+};
